98d642ce0cfa52b42a6fe80fcdfc8e16cbeae443
Ethan T <54526635+ethan2o@users.noreply.github.com>
2023-11-10 15:45:27 +1100

    JavaScript Exercises 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  output = (temperature - 32) * 5 / 9;
+  return Math.round(output * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  output = temperature * 9 / 5 + 32;
+  return Math.round(output * 10) / 10;
 };
+
 
 // Do not edit below this line
 module.exports = {
